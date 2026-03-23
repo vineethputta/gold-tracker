11a2ca2b4526844669101e2961c9eff30df8fae5
vineethputta <73684740+vineethputta@users.noreply.github.com>
2026-03-24 02:23:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+let triggers = [];
+
 async function fetchPrices() {
     const res = await fetch("/prices");
     const data = await res.json();
 
     document.getElementById("gold").innerText = data.gold;
     document.getElementById("silver").innerText = data.silver;
+
+    checkTrigger(data);
 }
 
-setInterval(fetchPrices, 5000);
+function setTrigger() {
+    const type = document.getElementById("type").value;
+    const price = document.getElementById("price").value;
+
+    triggers.push({ type, price });
+
+    alert("✅ Alert set!");
+}
+
+function checkTrigger(data) {
+    triggers.forEach(t => {
+        if (
+            (t.type === "gold" && data.gold >= t.price) ||
+            (t.type === "silver" && data.silver >= t.price)
+        ) {
+            alert(`🚨 ${t.type} reached ₹${t.price}`);
+        }
+    });
+}
+
+// update every second
+setInterval(fetchPrices, 1000);
 fetchPrices();
